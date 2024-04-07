258da4731fd6abb51edda20798a1ec58c50d2a9d
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2024-04-07 23:14:04 +0300

    Add project files.

---
+++ RedisPubSub/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection =await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("Mesaj:");
+    string message =Console.ReadLine();
+    await subscriber.PublishAsync("mychannel",message);
+}
